6972bc6e168eced6b6319f97081265adb692d896
Andre Entrekin <andre.entrekin@gmail.com>
2020-01-31 10:33:25 -0600

    added css

--- controllers/marketcheckapi.js
+++ controllers/marketcheckapi.js
@@ -3,13 +3,15 @@ module.exports = function (app) {
     app.get("/api/marketcheck", function (req, res) {
         var options = {
             method: 'GET',
-            url: 'http://api.marketcheck.com/v2/search/car/active?api_key=Ya546Kh7BRRrFkCKAeOJ5ShV6Mp1oJAD&year=2017',
+            url: 'http://api.marketcheck.com/v2/search/car/active?api_key=wOLQfAk15aAqNUfsrUEHFA25GTOhALLm&year=2017',
             headers: {
                 'host': 'marketcheck-prod.apigee.net',
             }
         };
         request(options, function (error, response) {
             var parse = JSON.parse(response.body)
+            console.log(parse);
+
 
             // console.log();
             // var images = parse.listings[0].media.photo_links[0]
